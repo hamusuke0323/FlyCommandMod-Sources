ac398cb7eaa7409aecdf4808b5cd1a48381c8c0f
hamusuke0323 <60880668+hamusuke0323@users.noreply.github.com>
2020-03-28 19:37:11 +0900

    Add files via upload

---
+++ java/com/flymod/hamusuke/Main.java
@@ -0,0 +1,31 @@
+package com.flymod.hamusuke;
+
+import com.flymod.hamusuke.command.CommandFlying;
+import com.flymod.hamusuke.proxy.CommonProxy;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION)
+public class Main {
+	public static final String MODID = "flycommand";
+	public static final String MODNAME = "FlyCommandMod";
+	public static final String VERSION = "3.0";
+	public static final String COMMON = "com.flymod.hamusuke.proxy.CommonProxy";
+	public static final String CLIENT = "com.flymod.hamusuke.proxy.ClientProxy";
+	//public static final String SERVER = "hamusuke.proxy.ServerProxy";
+
+	@Instance
+	public static Main instance;
+
+	@SidedProxy(clientSide = CLIENT, serverSide = COMMON)
+	public static CommonProxy proxy;
+
+	@EventHandler
+	public static void serverInit(FMLServerStartingEvent event) {
+		event.registerServerCommand(new CommandFlying());
+	}
+}
